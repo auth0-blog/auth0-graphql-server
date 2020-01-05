bf5c1f91769776b11d6460978d8bc0a347271bff
royderks <10717410+royderks@users.noreply.github.com>
2020-01-05 18:08:28 +0100

    Check if the value for  has already been set before creating a new mock MongoDB instance, to prevent creating a new instance on every function call

--- src/database.js
+++ src/database.js
@@ -7,43 +7,46 @@ async function startDatabase() {
   const mongo = new MongoMemoryServer();
   const mongoDBURL = await mongo.getConnectionString();
   const connection = await MongoClient.connect(mongoDBURL, {
-    useNewUrlParser: true
+    useNewUrlParser: true,
   });
-  database = connection.db();
 
-  await database.collection("events").insertMany([
-    {
-      id: 1,
-      title: "GraphQL Introduction Night",
-      description: "Introductionary night to GraphQL",
-      date: "2019-11-06T17:34:25+00:00",
-      attendants: [
-        {
-          id: 1,
-          name: "Peter",
-          age: 34
-        },
-        {
-          id: 2,
-          name: "Kassandra",
-          age: 23
-        }
-      ]
-    },
-    {
-      id: 2,
-      title: "GraphQL Introduction Night #2",
-      description: "Introductionary night to GraphQL",
-      date: "2019-11-06T17:34:25+00:00",
-      attendants: [
-        {
-          id: 3,
-          name: "Kim",
-          age: null
-        }
-      ]
-    }
-  ]);
+  if (!database) {
+    database = connection.db();
+
+    await database.collection("events").insertMany([
+      {
+        id: 1,
+        title: "GraphQL Introduction Night",
+        description: "Introductionary night to GraphQL",
+        date: "2019-11-06T17:34:25+00:00",
+        attendants: [
+          {
+            id: 1,
+            name: "Peter",
+            age: 34,
+          },
+          {
+            id: 2,
+            name: "Kassandra",
+            age: 23,
+          },
+        ],
+      },
+      {
+        id: 2,
+        title: "GraphQL Introduction Night #2",
+        description: "Introductionary night to GraphQL",
+        date: "2019-11-06T17:34:25+00:00",
+        attendants: [
+          {
+            id: 3,
+            name: "Kim",
+            age: null,
+          },
+        ],
+      },
+    ]);
+  }
 
   return database;
 }
